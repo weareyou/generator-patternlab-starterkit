51228faee837629e891bc2a41051a0ec818b34ca
Jeroen Kampinga <j.kampinga@live.com>
2016-03-10 15:21:34 +0100

    Create seperate task in postcss.js for stylelint.

--- app/templates/grunt/_postcss.js
+++ app/templates/grunt/_postcss.js
@@ -2,19 +2,30 @@ module.exports = function(grunt, data) {
     return {
         "options": {
             "map": true,
-            "syntax": require('postcss-scss'),
             "processors": [
                 require('autoprefixer')({browsers: data.config.browserlist}),
-                require('stylelint')
             ]
         },
         "dev": {
             "files": [{
                 "expand": true,
-                "cwd": "<%= paths.src.sass %>",
-                "src": ["**/*.scss"],
+                "cwd": "<%= paths.dest.css %>",
+                "src": ["**/*.css"],
                 "dest": "<%= paths.dest.css %>"
             }]
+        },
+        "stylelint": {
+            "options": {
+                "writeDest": false,
+                    "syntax": require('postcss-scss'),
+                    "processors": [
+                        require('stylelint'),
+                    ]
+                },
+            "expand": true,
+            "cwd": "<%= paths.src.sass %>",
+            "src": ["**/*.scss"],
+            "dest": "<%= paths.dest.css %>"
         }
     }
 };
