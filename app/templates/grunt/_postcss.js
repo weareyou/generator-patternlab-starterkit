986c02fed664f33648ce2e8c4b871a232d0fcc36
Jeroen Kampinga <j.kampinga@live.com>
2016-03-10 14:07:36 +0100

    Add Stylelint to Postcss process.

--- app/templates/grunt/_postcss.js
+++ app/templates/grunt/_postcss.js
@@ -2,15 +2,17 @@ module.exports = function(grunt, data) {
     return {
         "options": {
             "map": true,
+            "syntax": require('postcss-scss'),
             "processors": [
-                require('autoprefixer')({browsers: data.config.browserlist})
+                require('autoprefixer')({browsers: data.config.browserlist}),
+                require('stylelint')
             ]
         },
         "dev": {
             "files": [{
                 "expand": true,
-                "cwd": "<%= paths.dest.css %>",
-                "src": ["**/*.css"],
+                "cwd": "<%= paths.src.sass %>",
+                "src": ["**/*.scss"],
                 "dest": "<%= paths.dest.css %>"
             }]
         }
