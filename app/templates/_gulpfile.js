1739336cbbe8e137a253bb3a5717f435e0a3fee4
Bram Smulders <bram@bramsmulders.com>
2016-04-21 11:03:44 +0200

    Added connect task

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -9,7 +9,6 @@ global.__config = config = require('./config.json');
  * Require needed modules
  */
 var gulp = require('gulp');
-var gulpRequireTasks = require('gulp-require-tasks');
 var runSequence = require('run-sequence');
 var merge = require('gulp-merge-json');
 var shell = require('gulp-shell');
@@ -18,6 +17,8 @@ var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var browserSync = require('browser-sync').create();
+var del = del = require('del');
 
 
 /*----------------------------------------------------------------------------*\
@@ -26,6 +27,14 @@ var autoprefixer = require('autoprefixer');
 
 /*  Patternlab
 \*----------------------------------------------------------------------------*/
+
+/**
+ * Clean patterns dir
+ */
+gulp.task('pl-clean', function(cb){
+    del.sync([config.paths.public.patterns + '*'], {force: true});
+    cb();
+});
 
 /**
  * Merge json
@@ -112,6 +121,7 @@ gulp.task('styles', function(cb){
         .pipe(postcss(processors))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(config.paths.public.css))
+        .pipe(browserSync.stream({match: '**/*.css'}))
     ;
 });
 
@@ -120,6 +130,64 @@ gulp.task('styles', function(cb){
 /*  Runnable tasks
 \*----------------------------------------------------------------------------*/
 
+/**
+ * Connect
+ */
+gulp.task('connect', ['lab'], function() {
+    browserSync.init({
+        server: {
+            baseDir: config.paths.public.root
+        },
+        snippetOptions: {
+            // Ignore all HTML files within the templates folder
+            blacklist: ['/index.html', '/', '/?*']
+        },
+        notify: {
+            styles: [
+                'display: none',
+                'padding: 15px',
+                'font-family: sans-serif',
+                'position: fixed',
+                'font-size: 1em',
+                'z-index: 9999',
+                'bottom: 0px',
+                'right: 0px',
+                'border-top-left-radius: 5px',
+                'background-color: #1B2032',
+                'opacity: 0.4',
+                'margin: 0',
+                'color: white',
+                'text-align: center'
+            ]
+        }
+    });
+    gulp.watch(config.paths.source.sass + '**/*.scss', ['styles']);
+
+    // gulp.watch(config.paths.source.styleguide, '**/*.*', ['pl-copy:styleguide']);
+
+    var patternWatches = [
+        config.paths.source.patterns + '**/*.mustache',
+        config.paths.source.patterns + '**/*.json',
+        config.paths.source.data + '**/*.json',
+        config.paths.source.fonts + '**/*',
+        config.paths.source.images + '**/*'
+    ];
+
+    gulp.watch(patternWatches, ['lab-pipe'], function () { browserSync.reload(); });
+});
+
+gulp.task('lab-pipe', ['lab'], function(cb){
+    cb();
+    browserSync.reload();
+});
+
+gulp.task('prelab', ['pl-clean'/*, 'assets'*/]);
+gulp.task('lab', ['prelab', 'patternlab'], function(cb){cb();});
+
+
+/**
+ * Default task
+ */
 gulp.task('default', function(cb) {
     runSequence (
         ['merge-json'],
