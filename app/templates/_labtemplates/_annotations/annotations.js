608aafd34b4c149e59ccec179e9b9c1f0c7b728d
Bram Smulders <bram@bramsmulders.com>
2016-11-23 16:19:55 +0100

    Cleanup all code

--- app/templates/_labtemplates/_annotations/annotations.js
+++
@@ -1,9 +0,0 @@
-var comments = {
-    "comments" : [
-        {
-            "el": "#colors",
-            "title" : "Special Markup and Styling",
-            "comment": "This code uses pattern-scaffolding.css in the `source/css/` directory."
-        }
-    ]
-};
