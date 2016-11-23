86fc1887d6ed5191637d13bcda5aa41c7e5821d1
Bram Smulders <bram@bramsmulders.com>
2016-11-23 16:45:05 +0100

    Added basic prompts for the project settings

--- app/index.js
+++ app/index.js
@@ -1,9 +1,36 @@
 'use strict';
 
-var generators = require('yeoman-generator');
+const generators = require('yeoman-generator');
+const yosay = require('yosay');
 
 module.exports = generators.Base.extend({
-    helloworld: function () {
-        console.log('hello world');
+
+    /**
+     * Promts the user with some questions about the project
+     */
+    prompting: function () {
+        this.log(yosay('Welcome to the Patternlab starterkit generator!'));
+
+        return this.prompt([
+            {
+                type    : 'input',
+                name    : 'projectname',
+                message : 'What is the name of your project?',
+                default : 'Patternlab Starterkit' // Default to current folder name
+            }, {
+                type    : 'input',
+                name    : 'scaffoldPath',
+                message : 'Where do you want the patternlab folder to be generated?',
+                default : 'public'
+            }, {
+                type    : 'confirm',
+                name    : 'gitignore',
+                message : 'Do you want to copy gitignore?'
+            }
+        ]).then(function (answers) {
+            this.log('projectname', answers.projectname);
+            this.log('scaffoldPath', answers.projectname);
+            this.log('gitignore', answers.gitignore);
+        }.bind(this));
     }
 });
