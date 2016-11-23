eab0aa795a1aafad5bceb6097956a9cc8318ba50
Bram Smulders <bram@bramsmulders.com>
2016-11-23 16:25:38 +0100

    hello world!

--- app/index.js
+++ app/index.js
@@ -2,4 +2,9 @@
 
 var generators = require('yeoman-generator');
 
-module.exports = generators.Base.extend();
+module.exports = generators.Base.extend({
+  // The name `constructor` is important here
+  helloworld: function () {
+    console.log('hello world');
+  }
+});
