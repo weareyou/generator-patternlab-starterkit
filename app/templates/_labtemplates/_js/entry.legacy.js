b07d3427b858efd57dd450d2a838661e747d2270
Bram Smulders <bram@bramsmulders.com>
2019-07-04 15:52:33 +0200

    added element-closest polyfill

--- app/templates/_labtemplates/_js/entry.legacy.js
+++ app/templates/_labtemplates/_js/entry.legacy.js
@@ -2,6 +2,7 @@ import '@babel/polyfill';
 
 // Import legacy polyfills not provided by babel-polyfill.
 import 'svgxuse';
+import 'element-closest/browser';
 // import 'whatwg-fetch';
 
 // Import the main entry file after the polyfills.
